60fce7961d84b14dd8189a397cb8c8c67b8a772f
GiovannaAlbanezi <giovanna.albanezi@etec.sp.gov.br>
2025-02-25 18:17:14 -0300

    funcoes node

--- aula2/index.js
+++ aula2/index.js
@@ -4,13 +4,4 @@ function soma(n1, n2){
    
 console.log(soma(13, 63))
 
-function sub(n1-n2){
-    return(n1-n2)
-}
-function div(n1/n2){
-    return(n1/n2)
-}
-function mul(n1.n2){
-    return(n1.n2)
-}
 console.log()
